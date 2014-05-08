ae25494f8d55fe5f9c731f014e1139b36d1bb7b6
Sebastian <seabasstheSwordsman@gmail.com>
2014-05-08 12:26:05 -0700

    Add notes up.

--- PortfolioSite/Controllers/ProjectsController.cs
+++ PortfolioSite/Controllers/ProjectsController.cs
@@ -27,6 +27,7 @@ namespace PortfolioSite.Controllers
                 "/gallery4.png",
                 "/gallery5.png",
                 "/gallery6.png",
+                "/gallery7.png"
             };
             var mobileGallery = new List<String>()
             {
@@ -36,11 +37,37 @@ namespace PortfolioSite.Controllers
                 "/gallery4.png",
                 "/gallery5.png",
                 "/gallery6.png",
+                "/gallery7.png"
             };
             ViewBag.DesktopImagePath = desktopImagePath;
             ViewBag.DesktopGallery = desktopGallery;
             ViewBag.MobileImagePath = mobileImagePath;
             ViewBag.MobileGallery = mobileGallery;
+            return View();
+        }
+
+        public ActionResult Math115A()
+        {
+            return View();
+        }
+
+        public ActionResult Math115B()
+        {
+            return View();
+        }
+
+        public ActionResult Math125A()
+        {
+            return View();
+        }
+
+        public ActionResult Math135A()
+        {
+            return View();
+        }
+
+        public ActionResult Math135B()
+        {
             return View();
         }
 	}
